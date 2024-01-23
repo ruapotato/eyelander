19f2c702042bc7f9e8bb69f6d6ed96927c9159f7
David Hamner <david.hamner@puri.sm>
2024-01-22 20:43:11 -0800

    Better theme

--- Light_sounds.gd
+++ Light_sounds.gd
@@ -14,7 +14,7 @@ func _process(delta):
 		var this_stream = get_stream_playback()
 		var volume_peak = AudioServer.get_bus_peak_volume_left_db(0,0)
 		if volume_peak > -150:
-			light.light_energy = abs(volume_peak) / 10
+			light.light_energy = abs(volume_peak) / 40
 			#print(volume_peak)
 	else:
 		light.light_energy = 0
